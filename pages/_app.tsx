c4378791903d84a068a87c464ff97dab2e2b8604
sabasayer <salih.b.sayer@gmail.com>
2022-08-25 15:25:54 +0300

    feat: mui install

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { lightTheme } from '../styles/theme/light-theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+function MyApp(props: AppProps) {
+
+  const { Component,  pageProps } = props;
+
+  return <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+       
 }
 
 export default MyApp
